827069d82f18a27c81d470614f0eca593c2c91c0
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-05-06 16:15:46 -0600

    Finish task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numOfTimes) {
+    if(numOfTimes < 0) return "ERROR";
+    return stringToRepeat.repeat(numOfTimes);
 };
 
 // Do not edit below this line
