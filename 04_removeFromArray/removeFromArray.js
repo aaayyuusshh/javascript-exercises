cea06d5fce1420d66ccaf066815f9eed08c2baa3
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-05-06 17:04:09 -0600

    Finish task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, ...elementToRemove) {
+    // using for of loop
+    // const modifiedArray = [];
+    // for(const element of originalArray)  {
+    //     if (!elementToRemove.includes(element)) 
+    //         modifiedArray.push(element); 
+    // }
+   
+    // using filter
+    const modifiedArray =  originalArray.filter((element) => {
+        return !elementToRemove.includes(element)
+    });
+    return modifiedArray;
 };
 
 // Do not edit below this line
