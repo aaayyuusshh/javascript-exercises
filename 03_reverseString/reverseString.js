e6004f16e07a58319368cbb908234b6e8305d6bc
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-05-06 16:33:46 -0600

    Finish task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    // via built in methods
+    // let arrToReverse = stringToReverse.split("");
+    // let reversedArray = arrToReverse.reverse();
+    // let reversedString = reversedArray.join("");
 
+    // via manual implementation
+    let reversedString = "";
+    for(let i=0; i<stringToReverse.length; i++) {
+        reversedString+= stringToReverse[stringToReverse.length-1-i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
