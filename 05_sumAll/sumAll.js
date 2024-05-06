7a49ae36f1f8cfb8fe305a8da6e3bd7e1f1fd7d0
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-05-06 17:24:23 -0600

    Finish task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(rangeVal1, rangeVal2) {    
+    if(typeof(rangeVal1)!= "number" || typeof(rangeVal2)!== "number" || rangeVal1 < 0 || rangeVal2 < 0) {
+        return "ERROR";
+    }
+    let lowerBound=Math.min(rangeVal1, rangeVal2);
+    let upperBound = lowerBound === rangeVal1 ? rangeVal2 : rangeVal1; 
+    let totalSum = 0;
+    for(lowerBound; lowerBound<=upperBound; lowerBound++) {
+        totalSum += lowerBound;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
